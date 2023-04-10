f7c871e857b88d8c8d3ccfe022e5334b342eb430
NatSsy <ssyhakarnata24@gmail.com>
2023-04-10 17:19:26 -0300

    middlewares-validaciones

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
-const express = require ("express");
-const {crearUsuario, loginUsuario} = require("../controllers/auth");
-
+const express = require("express");
+const { crearUsuario, loginUsuario } = require("../controllers/auth");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validarCampos");
 const routerAuth = express.Router()
 
-routerAuth.post("/new", crearUsuario)
-routerAuth.post("/", loginUsuario )
+routerAuth.post("/new", [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El correo es obligatorio").not().isEmpty().isEmail(),
+    check("password", "La constraseña sebe ser mayor a 5 caracteres").isLength({ min: 5, }),
+    validarCampos,
+],
+    crearUsuario
+);
+routerAuth.post("/", loginUsuario)
 
 
 
-module.exports= routerAuth;
+module.exports = routerAuth;
